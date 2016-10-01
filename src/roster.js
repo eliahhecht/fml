f0bd6f7d9871b8b0ef1baffdc05bd65a78933f77
Eliah Hecht <eliahhecht@gmail.com>
2016-10-01 17:00:05 -0400

    add position column

--- src/roster.js
+++ src/roster.js
@@ -1,4 +1,5 @@
 var mtgJson = require('./mtgjson')
+var _ = require('lodash')
 
 var loadCardsOwnedByPlayer = function(memberId) {
   // mocked for now
@@ -9,32 +10,34 @@ var loadCardsOwnedByPlayer = function(memberId) {
   ]
 }
 
+var isLand = function(cardData) {
+  return _.includes(cardData.types, 'Land')
+}
+
+var isPermanent = function(cardData) {
+  return _.intersection(cardData.types, ['Land', 'Artifact', 'Enchantment', 'Creature', 'Planeswalker']).length > 0
+}
+
 var constructRosterData = function(cardOwnership) {
   var rosterData = { name: cardOwnership.name }
   if (cardOwnership.status == 'bench') {
-    rosterData.position = 'bench'
+    rosterData.position = 'Bench'
   } else {
     var cardData = mtgJson.getCardByName(cardOwnership.name)
-    
+    if (isLand(cardData)) {
+      rosterData.position = 'Land'
+    } else if (isPermanent(cardData)) {
+      rosterData.position = 'Permanent'
+    } else {
+      rosterData.position = 'Non-Permanent'
+    }
   }
-
+  return rosterData
 }
 
 var loadRoster = function(memberId) {
   var ownedCards = loadCardsOwnedByPlayer(memberId)
-  var roster = {
-    active: [],
-    bench: []
-  }
-  for (var card of ownedCards) {
-    var cardData = mtgJson.getCardByName(card.name)
-    if (card.status == 'active') {
-      roster.active.push(cardData)
-    } else {
-      roster.bench.push(cardData)
-    }
-  }
-  return roster
+  return _.map(ownedCards, constructRosterData)
 }
 
 exports.loadRoster = loadRoster;
