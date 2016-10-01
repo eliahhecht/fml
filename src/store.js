98624f24e3019ecbd772f2f2a43bdffe1248e7d7
mpaulweeks <mpaulweeks@gmail.com>
2016-10-01 15:45:43 -0400

    dont insert test data on every app start

--- src/store.js
+++ src/store.js
@@ -13,21 +13,21 @@ var db = new sqlite3.Database(file);
 db.serialize(function() {
   if(!exists) {
     db.run("CREATE TABLE Stuff (thing TEXT)");
-  }
   
-  var stmt = db.prepare("INSERT INTO Stuff VALUES (?)");
+    var stmt = db.prepare("INSERT INTO Stuff VALUES (?)");
 
-  //Insert random data
-  var rnd;
-  for (var i = 0; i < 10; i++) {
-    rnd = Math.floor(Math.random() * 10000000);
-    stmt.run("Thing #" + rnd);
+    //Insert random data
+    var rnd;
+    for (var i = 0; i < 10; i++) {
+      rnd = Math.floor(Math.random() * 10000000);
+      stmt.run("Thing #" + rnd);
+    }
+    stmt.finalize();
   }
 
-  stmt.finalize();
-    db.each("SELECT rowid AS id, thing FROM Stuff", function(err, row) {
-      console.log(row.id + ": " + row.thing);
-    });
+  db.each("SELECT rowid AS id, thing FROM Stuff", function(err, row) {
+    console.log(row.id + ": " + row.thing);
+  });
 });
 
 db.close();
